b963c8fcbd21ee1afccf08bf1e8b8aeac314ede8
calin014 <calin.avasilcai@gmail.com>
2014-11-13 11:18:04 +0200

    separate frontends

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/client'))
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
